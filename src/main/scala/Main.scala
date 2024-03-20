34a4f59ad3081f6631038fa17793e56dacf2da1f
IvanMaslov52 <ivanmaslov19911@gmail.com>
2024-03-20 22:43:26 +0400

    make homework3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,19 @@
-import module1.{hof, type_system}
-
+import module1.homework.collections.Urn
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
 
-    val f: String => Unit = hof.logRunningTime(hof.doomy)
-    f("ooops")
+    println("Вероятность появление хотя бы одного шара равна 0.8")
+    val start = System.currentTimeMillis();
+    val bucket = Vector.fill(10000)(Urn())
+    val result = bucket.map(_.drawBalls()).count({ result => result }) / 10000.0
+    println(s"Вероятность появление хотябы одного шара в результате эксперимента равна: $result")
+    val end = System.currentTimeMillis()
+    println("Разница: " + (0.8 - result))
+    println("Running time: " + (end - start))
   }
+
+
+  def sum(x: Int, y: Int): Int = x + y
 }
