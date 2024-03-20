25eee805759c1566cff0618d4d83a4b751c316be
IvanMaslov52 <ivanmaslov19911@gmail.com>
2024-03-20 23:02:56 +0400

    fix main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,11 @@ import module1.homework.collections.Urn
 object Main {
 
   def main(args: Array[String]): Unit = {
-     println("Вероятность появление хотя бы одного шара равна 0.8")
+     println("Вероятность появление хотя бы одного белого шара равна 0.8")
      val start = System.currentTimeMillis()
      val bucket = Vector.fill(10000)(Urn())
      val result = bucket.map(_.drawBalls()).count({ result => result }) / 10000.0
-     println(s"Вероятность появление хотябы одного шара в результате эксперимента равна: $result")
+     println(s"Вероятность появление хотябы одного белого шара в результате эксперимента равна: $result")
      val end = System.currentTimeMillis()
      println("Разница: " + (0.8 - result))
      println("Running time: " + (end - start))
